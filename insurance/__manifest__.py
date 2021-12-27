d3257701097f49080f8464ab1c5ee2544b5affdd
hnadi-gnan <hnadi.gnan@gmail.com>
2021-12-27 10:55:36 +0000

    first trail

---
+++ insurance/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'insurance',
+    'sequence': 20,
+
+    'depends': ['base', 'mail'],
+    'data': [
+        'views/views.xml',
+    ],
+    'application': True,
+    'license': 'LGPL-3',
+}
